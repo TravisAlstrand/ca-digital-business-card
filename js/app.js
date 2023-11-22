80ba5bf67e7ecb21d22aa4e7c1992d47910e1cc7
Travis Alstrand <travisalstrand@gmail.com>
2023-11-22 15:36:03 -0800

    further mobile animations / aria attributes

--- js/app.js
+++ js/app.js
@@ -1,26 +1,78 @@
+// Header Elements
 const logo = document.querySelector("#logo");
-const container = document.querySelector("#container");
-const turnBtn = document.querySelector("#turnBtn");
-const turnSVG = document.querySelector("#turnBtnSVG");
-const nameSocials = document.querySelector("#nameSocials");
-const socials = document.querySelector("#socials");
+const frontNameCont = document.querySelector("#frontNameSocials");
+const backNameCont = document.querySelector("#backNameSocials");
 
 window.addEventListener("load", () => {
-  addSpinAnim(logo);
+  addSpinAnim(logo, "right");
 });
 
+// Card Elements
+const container = document.querySelector("#container");
+const cardFront = container.querySelector("#cardFront");
+const cardBack = container.querySelector("#cardBack");
+const turnBtn = document.querySelector("#turnBtn");
+const turnSVG = document.querySelector("#turnBtnSVG");
+
+// Which side is visible?
+let isFrontVisible = true;
+
+// Flip the card
 turnBtn.addEventListener("click", () => {
-  socials.classList.toggle("flex");
-  socials.classList.toggle("no-display");
-  nameSocials.classList.toggle("justify-center");
-  container.firstElementChild.classList.toggle("flip");
+  if (isFrontVisible) {
+    fadeInOutHeader(frontNameCont, backNameCont);
+    setTimeout(() => {
+      addSpinAnim(logo, "right");
+    }, 500);
+  } else {
+    fadeInOutHeader(backNameCont, frontNameCont);
+    setTimeout(() => {
+      addSpinAnim(logo, "left");
+    }, 500);
+  }
+  switchAriaHiddens();
+  toggleClass(container.firstElementChild, "flip");
   addSpinAnim(turnSVG);
-  console.log("turn card over");
 });
 
-function addSpinAnim(el) {
-  el.style.animation = "spin 1s ease-in-out";
+function switchAriaHiddens() {
+  isFrontVisible = cardFront.getAttribute("aria-hidden") === "true";
+
+  cardFront.setAttribute("aria-hidden", !isFrontVisible);
+  frontNameCont.setAttribute("aria-hidden", !isFrontVisible);
+  cardBack.setAttribute("aria-hidden", isFrontVisible);
+  backNameCont.setAttribute("aria-hidden", !isFrontVisible);
+}
+
+function toggleClass(el, cssClass) {
+  el.classList.toggle(cssClass);
+}
+
+function fadeInOutHeader(fromEl, toEl) {
+  fromEl.style.animation = "fadeOut .5s ease-in-out";
   setTimeout(() => {
-    el.style.animation = null;
-  }, 1100);
+    fromEl.style.opacity = 0;
+    fromEl.style.animation = null;
+    toggleClass(fromEl, "no-display");
+    toggleClass(toEl, "no-display");
+    toEl.style.animation = "fadeIn .5s ease-in-out";
+    setTimeout(() => {
+      toEl.style.animation = null;
+      toEl.style.opacity = 1;
+    }, 500);
+  }, 500);
+}
+
+function addSpinAnim(el, direction) {
+  if (direction === "right") {
+    el.style.animation = "spinRight 1s ease-in-out";
+    setTimeout(() => {
+      el.style.animation = null;
+    }, 1100);
+  } else {
+    el.style.animation = "spinLeft 1s ease-in-out";
+    setTimeout(() => {
+      el.style.animation = null;
+    }, 1100);
+  }
 }
